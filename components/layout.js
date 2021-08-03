964b4abafb476905b36d52b0309e411bd2198a29
thenolin <nateolin17@gmail.com>
2021-08-03 17:40:34 -0500

    test: move profile up directory

--- components/layout.js
+++ components/layout.js
@@ -16,21 +16,21 @@ export default function Layout({ children, home }) {
           name="description"
           content="Learn how to build a personal website using Next.js"
         />
-        <meta
+        {/* <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary_large_image" /> */}
       </Head>
       <header className={styles.header}>
         {home ? (
           <>
             <Image
               priority
-              src="/images/IMG_8673med.jpg"
+              src="/IMG_8673med.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
